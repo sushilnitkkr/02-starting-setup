bbd3f6e516fd1dc18292ee835c4fe974f8c69546
sushil kumar <isushilchaudhary@gmail.com>
2023-07-04 09:43:57 +0530

    first commit

---
+++ src/components/Concepts.js
@@ -0,0 +1,15 @@
+
+const Concepts=(props)=>{
+    return(
+    
+        <li className="concept">
+          <img src={props.image} alt="TODO: TITLE" />
+          <h2>{props.title}</h2>
+          <p>{props.description}</p>
+        </li>
+      
+    
+    );
+}
+
+export default Concepts;
